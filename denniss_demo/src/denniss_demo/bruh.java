bed5709a0cdf2a1a4498c31e6f5911226d31ee5a
1100100824 <1100100824@L815WD220005.edu.cbe.ab.ca>
2023-09-12 09:43:49 -0600

    9:43 am 9/12/2023

---
+++ denniss_demo/src/denniss_demo/bruh.java
@@ -0,0 +1,21 @@
+package denniss_demo;
+
+public class bruh {
+
+	public static void main(String[] args) {
+		
+		System.out.print("welcome to the jungle  \t");
+		System.out.println("you should kys NOW");
+		int age;
+		int random;
+		double number;
+	    String name;
+	    name = "donke";
+		random = 15;
+		age = 18+random;
+		number = 5;
+		System.out.println(age);
+		System.out.println(number);
+	}
+
+}
---
+++ denniss_demo/src/denniss_demo/denniss_demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package denniss_demo;
+
+/**
+ * @author 1100100824
+ *
+ */
+public class denniss_demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
