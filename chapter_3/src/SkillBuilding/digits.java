8c0407274920b4f642f12300c1bdf7b4d557e6da
1100100824 <1100100824@L815WD220005.edu.cbe.ab.ca>
2023-09-21 10:20:25 -0600

    9/21/2023

--- chapter_3/src/SkillBuilding/digits.java
+++ chapter_3/src/SkillBuilding/digits.java
@@ -1,10 +1,29 @@
+/*
+
+Program: digits.java          Last Date of this Revision: September 21, 2023
+
+Purpose: An application that calculates that displays ones place and tens place
+
+Author: Dennis Alacahanli, 
+School: CHHS
+Course: Computer Science 20 ??
+ 
+
+*/
 package SkillBuilding;
+
+import java.util.Scanner;
 
 public class digits {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner scanner = new Scanner(System.in); // create a scanner
+		System.out.println("type in a number of 2 digits" ); // ask for a 2 digit number
+		String number = scanner.nextLine(); // take input in string format
+		   
+		System.out.println("tens place is:" + number.charAt(0) ); // display tens
+		System.out.println("ones place is:" + number.charAt(1) ); // displays ones
 
 	}
 
---
+++ chapter_3/src/SkillBuilding/grade_avg_part1.java
@@ -0,0 +1,38 @@
+/*
+
+Program: grade_avg_part_1.java          Last Date of this Revision: September 21, 2023
+
+Purpose: An application that calculates the average of 5 grades
+
+Author: Dennis Alacahanli, 
+School: CHHS
+Course: Computer Science 20 ??
+ 
+
+*/
+package SkillBuilding;
+
+import java.util.Scanner;
+
+public class grade_avg_part1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("type in the first lesson grade:"); // ask user for first grade input
+		int first = scanner.nextInt(); // take the first grade input
+		System.out.println("type in the second lesson grade:"); // ask user for second grade input
+		int second = scanner.nextInt(); // take the second grade input
+		System.out.println("type in the third lesson grade:"); // ask user for third grade input
+		int third = scanner.nextInt(); // take the third grade input 
+		System.out.println("type in the fourht lesson grade:"); // ask user for fourth grade input
+		int fourth = scanner.nextInt(); // take the fourth grade input
+		System.out.println("type in the fifth lesson grade:"); // ask user for fifth grade input
+		int fifth = scanner.nextInt(); // take the fifth grade input
+		int result; // define the result variable
+		result = (first + second + third + fourth + fifth)/ 5; // calculate the grade average
+		System.out.println("your grade average is " + result); // display the grade average
+
+	}
+
+}
